105cd08c28eb15fc9a6c46594df3f869707cb67b
Test1 <quanchomsao@gmail.com>
2023-06-05 17:13:17 +0700

    Updated code

--- open.js
+++ open.js
@@ -3,7 +3,16 @@ const puppeteer = require('puppeteer-core');
 const { Cluster } = require('puppeteer-cluster');
 const XLSX = require('xlsx');
 
-const results = [];
+const run = async () => {
+  const results = [];
+
+  const browser = await puppeteer.launch({
+    headless: "new",
+    defaultViewport: null,
+    executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+    args: ['--incognito'],
+  });
+}
 
 // Đường dẫn đến tệp danh sách User Agents
 const userAgentsFile = 'user_agents.txt';
@@ -15,31 +24,26 @@ const userAgentsList = fs
   .map((line) => line.trim())
   .filter((line) => line.length > 0);
 
-function getRandomUserAgent() {
-  return userAgentsList[Math.floor(Math.random() * userAgentsList.length)];
-}
+
+const inputFile = 'uid_list.txt';
+const outputFile = 'results.xlsx';
+
+const uidList = fs
+  .readFileSync(inputFile, 'utf-8')
+  .split('\n')
+  .map((line) => line.trim())
+  .filter((line) => line.length > 0)
+  .map((uid) => `https://www.facebook.com/${uid}`);
 
 (async () => {
-  const inputFile = 'uid_list.txt';
-  const outputFile = 'results.xlsx';
-
-  const uidList = fs
-    .readFileSync(inputFile, 'utf-8')
-    .split('\n')
-    .map((line) => line.trim())
-    .filter((line) => line.length > 0);
-
   const cluster = await Cluster.launch({
     concurrency: Cluster.CONCURRENCY_CONTEXT,
-    maxConcurrency: 10,
+    maxConcurrency: 3,
     puppeteerOptions: {
-      headless: false,
+      headless: true,
       defaultViewport: null,
       executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
-      args: [
-        '--disable-infobars',
-        '--incognito',
-      ],
+      args: ['--incognito'],
     },
   });
 
@@ -47,42 +51,28 @@ function getRandomUserAgent() {
     console.error(`Error occurred while processing URL: ${data}: ${err.message}`);
   });
 
-  await cluster.task(async ({ page: taskPage, data: url, worker }) => {
-    const uid = url.replace('https://www.facebook.com/', '');
+  cluster.task(async ({ page, data: url }) => {
 
-    // Chọn một User Agent ngẫu nhiên từ danh sách User Agents
-    const userAgent = getRandomUserAgent();
+    await page.goto(url, { waitUntil: 'networkidle0' });
 
-    const page = await taskPage.browser().newPage();
+    const aboutURL = `${url}/about`;
+    await page.goto(aboutURL);
 
-    await page.setUserAgent(userAgent);
-
-    await page.waitForTimeout(Math.floor(Math.random() * 10000) + 5000);
-
-    await page.goto(url);
-
-    await page.waitForTimeout(5000);
-
-    const result = await page.evaluate((url) => {
+    const result = await page.evaluate(() => {
       const hasText = (element) => element && element.textContent.trim().length > 0;
-
-      const description = Array.from(document.querySelectorAll('span[style*="-webkit-box-orient: vertical;"]')).find(e => e.textContent.includes('Intro'));
+      const description = Array.from(document.querySelectorAll('span[style="-webkit-box-orient: vertical;"]')).find(e => e.textContent.includes('Intro'));
       const likeCountElement = Array.from(document.querySelectorAll('[tabindex="0"]')).find(e => e.textContent.includes('likes'));
       const followCountElement = Array.from(document.querySelectorAll('[tabindex="0"]')).find(e => e.textContent.includes('followers'));
-
-      const emailRegExp = /[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}/;
-      const emailElement = Array.from(document.querySelectorAll('*')).find(e => e.textContent.match(emailRegExp));
-      const phoneNumberRegExp = /(\+84|0)[0-9]{9,10}/;
+      const emailRegExp = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,6}/;
+      const emailElement = Array.from(document.querySelectorAll('')).find(e => e.textContent.match(emailRegExp));
+      const phoneNumberRegExp = /some-regular-expression-for-phone-number/;
       const phoneNumberElement = Array.from(document.querySelectorAll('*')).find(e => e.textContent.match(phoneNumberRegExp));
-
-      const addressElement = Array.from(document.querySelectorAll('[data-pagelet="ProfileAppSection_0"]')).find(e => e.querySelector('span span').textContent.includes('Address'));
-      const businessHoursElement = Array.from(document.querySelectorAll('[data-pagelet="ProfileAppSection_0"]')).find(e => e.querySelector('span span').textContent.includes('Hours'));
-      const websiteElement = Array.from(document.querySelectorAll('a')).find(e => e.href.includes(url) && e.textContent.includes('http'));
-
+      const addressElement = Array.from(document.querySelectorAll('[data-pagelet="ProfileAppSection_0"]')).find(e => e.querySelector('span span').textContent.includes('Địa chỉ'));
+      const businessHoursElement = Array.from(document.querySelectorAll('[data-pagelet="ProfileAppSection_0"]')).find(e => e.querySelector('span span').textContent.includes('Giờ hoạt động'));
+      const websiteElement = Array.from(document.querySelectorAll('a')).find(e => e.href.includes(url) && e.textContent.includes('Trang web'));
       const avatar = document.querySelector('circle[fill="white"]');
       const cover = document.querySelector('img[data-imgperflogname="profileCoverPhoto"]');
-
-      const username = new URL(window.location.href).pathname.slice(1).replace(/\/$/, '');
+      const username = new URL(win  dow.location.href).pathname.slice(1).replace;
 
       return {
         url,
@@ -94,21 +84,32 @@ function getRandomUserAgent() {
         sdt: phoneNumberElement ? phoneNumberElement.textContent.match(phoneNumberRegExp)[0] : 'không có',
         diaChi: addressElement ? addressElement.querySelector('div[role="presentation"]').textContent.trim() : 'không có',
         gioHoatDong: businessHoursElement && businessHoursElement.querySelector('div[role="presentation"]').style.color === '#31a24c' ? businessHoursElement.querySelector('div[role="presentation"]').textContent.trim() : 'không có',
-        website: websiteElement && websiteElement.style.color === 'hsl(214, 89%, 52%)' ? websiteElement.href : 'không có',
+        website: websiteElement && websiteElement.style.color === 'có' ? websiteElement.href : 'không có',
         avatar: avatar ? 'có' : 'không có',
         bia: cover ? 'có' : 'không có',
       };
-    }, url);
+    });
 
     console.log(result);
 
     results.push(result);
-
-    await page.close();
   });
 
-  for (const uid of uidList) {
-    cluster.queue('https://www.facebook.com/' + uid);
+
+const results = [];
+  for (let i = 0; i < uidList.length; i++) {
+    const uid = uidList[i];
+
+    // Chọn một User Agent ngẫu nhiên từ danh sách User Agents
+    const userAgent = getRandomUserAgent();
+
+    cluster.queue(uid, uid => uid);
+
+    if ((i + 1) % 100 === 0) {
+      await cluster.idle();
+      await cluster.close();
+      await cluster.launch();
+    }
   }
 
   await cluster.idle();
@@ -119,3 +120,12 @@ function getRandomUserAgent() {
   XLSX.utils.book_append_sheet(wb, ws, 'Kết quả');
   XLSX.writeFile(wb, outputFile);
 })();
+
+function getRandomUserAgent() {
+  return userAgentsList[Math.floor(Math.random() * userAgentsList.length)];
+}
+
+
+run().catch(error => {
+  console.error(error);
+});
